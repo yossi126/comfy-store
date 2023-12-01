2eeba060c1541182e8f9c2509714761e9a3bbe93
yossi126 <yossi126@gmail.com>
2023-12-02 01:05:24 +0200

    redux set up

--- compy-store/src/main.jsx
+++ compy-store/src/main.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "react-toastify/dist/ReactToastify.css";
+// order
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { ToastContainer } from "react-toastify";
+import { store } from "./store";
+import { Provider } from "react-redux";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+    <ToastContainer position="top-center" />
+  </Provider>
+);
--- compy-store/src/pages/SingleProduct.jsx
+++ compy-store/src/pages/SingleProduct.jsx
@@ -7,6 +7,8 @@ import {
   generateAmountOptions,
 } from "../utils/index";
 
+import { useDispatch } from "react-redux";
+import { addItem } from "../features/cart/cartSlice";
 export const loader = async ({ params }) => {
   const url = `/products/${params.id}`;
   const response = await customFetch(url);
@@ -23,9 +25,25 @@ const SingleProduct = () => {
   const dollarsAmount = formatPrice(price);
   const [productColor, setProductColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
+  const dispatch = useDispatch();
+
+  const cartProduct = {
+    cartID: product.id + productColor,
+    productID: product.id,
+    image,
+    title,
+    price,
+    amount,
+    productColor,
+    company,
+  };
 
   const handleAmount = (e) => {
     setAmount(parseInt(e.target.value));
+  };
+
+  const addToCart = () => {
+    dispatch(addItem({ product: cartProduct }));
   };
   return (
     <section>
@@ -96,10 +114,7 @@ const SingleProduct = () => {
           </div>
           {/* CART BUTTON */}
           <div className="mt-10 ">
-            <button
-              className="btn btn-secondary btn-md"
-              onClick={() => console.log("add to bag")}
-            >
+            <button className="btn btn-secondary btn-md" onClick={addToCart}>
               Add to bag
             </button>
           </div>
