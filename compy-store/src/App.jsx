78591fe8a66def94a5646523337c21eeeff492cc
yossi126 <yossi126@gmail.com>
2023-12-03 22:06:09 +0200

    login page

--- compy-store/src/App.jsx
+++ compy-store/src/App.jsx
@@ -23,6 +23,10 @@ import { loader as productsLoader } from "./pages/Products";
 
 //actions
 import { action as actionRegister } from "./pages/Register";
+import { action as loginAction } from "./pages/Login";
+import { store } from "./store";
+
+store;
 
 const router = createBrowserRouter([
   {
@@ -68,6 +72,7 @@ const router = createBrowserRouter([
     path: "/login",
     element: <Login />,
     errorElement: <Error />,
+    action: loginAction(store),
   },
   {
     path: "/register",
--- compy-store/src/features/user/userSlice.js
+++ compy-store/src/features/user/userSlice.js
@@ -5,6 +5,9 @@ const themes = {
   winter: "winter",
   dracula: "dracula",
 };
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("user")) || null;
+};
 
 const getThemeFromLocalStorage = () => {
   const theme = localStorage.getItem("theme") || themes.winter;
@@ -13,7 +16,7 @@ const getThemeFromLocalStorage = () => {
 };
 
 const initialState = {
-  user: { username: "coding addict" },
+  user: getUserFromLocalStorage(),
   theme: getThemeFromLocalStorage(),
 };
 
@@ -22,7 +25,9 @@ const userSlice = createSlice({
   initialState,
   reducers: {
     loginUser: (state, action) => {
-      console.log("login");
+      const user = { ...action.payload.user, token: action.payload.jwt };
+      state.user = user;
+      localStorage.setItem("user", JSON.stringify(user));
     },
     logoutUser: (state) => {
       state.user = null;
